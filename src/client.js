0e48f706f3122596cd56b4a712872d1f433de293
Dileep Kumar <dileep_bhimineni@yahoo.com>
2018-04-29 11:51:53 -0400

    init commit

---
+++ src/client.js
@@ -0,0 +1,76 @@
+/**
+ * THIS IS THE ENTRY POINT FOR THE CLIENT, JUST LIKE server.js IS THE ENTRY POINT FOR THE SERVER.
+ */
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createStore from './redux/create';
+import io from 'socket.io-client';
+import { Provider } from 'react-redux';
+import ApiClient from './helpers/ApiClient.js';
+import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { ReduxAsyncConnect } from 'redux-async-connect';
+import useScroll from 'scroll-behavior/lib/useStandardScroll';
+
+import getRoutes from './routes';
+
+const client = new ApiClient();
+const _browserHistory = useScroll(() => browserHistory)();
+const dest = document.getElementById('content');
+const store = createStore(_browserHistory, client, window.__data);
+const history = syncHistoryWithStore(_browserHistory, store);
+
+function initSocket() {
+  let socket = io('', {path: '/ws'});
+  let RETRY_INTERVAL = 5000;
+  let intervalTimer = null;
+  
+  socket.on('snapshot', (data) => {
+    console.log(data);
+    socket.emit('my other event', { my: 'data from client' });
+  });
+  socket.on('update', (data) => {
+    console.log(data);
+  });
+  socket.on('close', () => {
+      console.log('connection closed');
+  });
+  socket.on('disconnect',() => {
+      /*on disconnect timer will try to reconnect using interval can also implement using
+           io.connect({
+               reconnection: true,
+               reconnectionDelay: 1000,
+               reconnectionDelayMax : 5000,
+               reconnectionAttempts: 99999
+           })
+       */
+      
+      intervalTimer = window.setInterval(() => {
+          if (socket.connected) {
+              clearInterval(intervalTimer);
+              intervalTimer = null;
+              return;
+          }
+          socket.connect();
+      }, RETRY_INTERVAL);
+  });
+  return socket;
+}
+global.socket = initSocket();
+
+const component = (
+  <Router
+    render={(props) =>
+        <ReduxAsyncConnect {...props} helpers={{client}} filter={item => !item.deferred} />
+      } history={history}>
+    {getRoutes(store)}
+  </Router>
+);
+
+ReactDOM.render(
+  <Provider store={store} key="provider">
+    {component}
+  </Provider>,
+  dest
+);
---
+++ src/containers/App/App.js
@@ -0,0 +1,154 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Cell, Column, ColumnGroup, Table } from 'fixed-data-table';
+import '../../../node_modules/fixed-data-table/dist/fixed-data-table.css';
+import _ from 'lodash';
+import './app.css';
+
+@connect(
+    state => ({rows: state.rows, cols: state.cols || new Array(10)})
+)
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: [],
+      cols: new Array(10),
+      secondsElapsed: 0,
+    };
+    this.onSnapshotReceived = this.onSnapshotReceived.bind(this);
+    this.onUpdateReceived = this.onUpdateReceived.bind(this);
+    this._cell = this._cell.bind(this);
+    this._headerCell = this._headerCell.bind(this);
+    this._generateCols = this._generateCols.bind(this);
+    this.shouldUpdate = true;
+  }
+  
+  //update entries twice a second
+  shouldComponentUpdate() {
+    if(!this.shouldUpdate) {
+      return false;
+    }
+    
+    this.shouldUpdate = false;
+    setTimeout(() =>  this.shouldUpdate = true, 5000);
+    return true;
+  }
+    
+  onSnapshotReceived(data) {
+    let rows = [];
+    data.forEach(row => {
+      rows[row.id] = row;
+    });
+    // const rows = this.state.rows.concat(data);
+    console.log('snapshot' + rows);
+    const cols = Object.keys(rows[0]);
+    this.setState({rows, cols});
+  };
+  onUpdateReceived(data) {
+    // const rows = this.state.rows.concat(data);
+
+    let rows = this.state.rows;
+    data.forEach(newRow => {
+      rows[newRow.id] = newRow;
+    });
+
+    this.setState({rows});
+  };
+  _cell(cellProps) {
+    const rowIndex = cellProps.rowIndex;
+    const rowData = this.state.rows[rowIndex];
+    const col = this.state.cols[cellProps.columnKey];
+    const content = rowData[col];
+    return (
+      <CellWithUpdates content={content} />
+    );
+  }
+
+  _headerCell(cellProps) {
+    const col = this.state.cols[cellProps.columnKey];
+    return (
+      <Cell>{col}</Cell>
+    );
+  }
+
+  _generateCols() {
+    console.log('generating...');
+    let cols = [];
+    this.state.cols.forEach((row, index) => {
+      cols.push(
+        <Column
+          width={100}
+          flexGrow={1}
+          cell={this._cell}
+          header={this._headerCell}
+          columnKey={index}
+          />
+      );
+    });
+    console.log(cols);
+    return cols;
+  };
+  componentDidMount() {
+    if (socket) {
+      socket.on('snapshot', this.onSnapshotReceived);
+      socket.on('updates', _.throttle(this.onUpdateReceived, 500));
+    }
+  };
+  componentWillUnmount() {
+    if (socket) {
+      socket.removeListener('snapshot', this.onSnapshotReceived);
+      socket.removeListener('updates', this.onUpdateReceived);
+    }
+  };
+
+  render() {
+    const columns = this._generateCols();
+    return (
+      <Table
+        rowHeight={30}
+        width={window.innerWidth}
+        maxHeight={window.innerHeight}
+        headerHeight={35}
+        rowsCount={this.state.rows.length}
+        >
+        {columns}
+      </Table>
+    );
+  }
+}
+
+class CellWithUpdates extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: null
+    }
+  }
+  
+  componentWillReceiveProps(newProps){
+    if(this.props.content !== newProps.content) {
+      let style = null;
+      if(parseInt(this.props.content) < parseInt(newProps.content)) {
+        style = { color: 'red'}
+      }
+  
+      if(parseInt(this.props.content) > parseInt(newProps.content)) {
+        style = { color: 'green' }
+      }
+  
+      this.setState({
+        style
+      })
+    }
+  }
+  
+  render() {
+    return(
+      <Cell style={{ color: '#333', width: '100%', ...this.state.style  }}>{this.props.content}</Cell>
+    )
+  }
+}
